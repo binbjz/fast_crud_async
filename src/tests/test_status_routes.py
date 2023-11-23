155b84148279e26dea8b81d392de4b54cb63dd4b
binbjz <8213913+binbjz@users.noreply.github.com>
2023-11-23 14:40:25 +0800

    Update and rename test_rt_chk.py to test_status_routes.py

--- src/tests/test_status_routes.py
+++ src/tests/test_status_routes.py
@@ -1,4 +1,4 @@
-def test_route_check(test_app):
-    response = test_app.get("/rt_chk")
+def test_status_check(test_app):
+    response = test_app.get("/status_check")
     assert response.status_code == 200
     assert response.json() == {"verify": "success!"}
